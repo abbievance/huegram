936e43cdaf4a0aa266d8a03daa014f2da099d299
Abbie Vance <89212624+abbievance@users.noreply.github.com>
2023-12-09 21:25:12 -0600

    Fix some spacing issues

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -3,6 +3,7 @@ import HueObject from '../HueObject'
 import { IoIosSearch } from "react-icons/io";
 import { RiHashtag } from "react-icons/ri";
 import { useState } from 'react';
+import "./Header.css"
 
 interface Props {
     hues: HueObject[],
@@ -33,14 +34,14 @@ const Main = (props : Props) => {
 
   return (
     <div className='flex flex-col justify-start items-center border-l-2 border-r-2 w-3/4'>
-      <div className="flex flex-col lg:flex-row fixed w-1/2 justify-between items-center mt-2">
+      <div className="flex flex-col lg:flex-row fixed w-1/2 justify-between items-center mt-2 header">
 
         <div className='flex mt-2 justify-center items-center text-white text-4xl'>
             <h1 className='text-center'><RiHashtag /></h1>
             <h1 className='text-center'>Huegram</h1>
         </div>
 
-        <div className='flex mt-2 justify-center items-center w-full sm:w-1/4'>
+        <div className='flex mt-2 justify-center items-center w-full sm:w-1/4 mr-6 search'>
             <input type="text" placeholder='Search Hues...' name="search" id="search" className='text-slate-800 rounded-lg p-1' value={searchHue}
             onChange={(e) => {
               setSearchHue(e.target.value), filterHues()
@@ -49,7 +50,7 @@ const Main = (props : Props) => {
         </div>
       </div>
    
-      <div className='flex flex-wrap w-full justify-center gap-8 overflow-y-auto lg:mt-20 mt-36'>
+      <div className='flex flex-wrap w-full justify-center gap-8 overflow-y-auto lg:mt-20 mt-36 hues'>
         <div>
               {searchCondition() ? <div className='flex flex-wrap gap-8 justify-center'> 
                   {filteredHues.map(  (hue) => (          
