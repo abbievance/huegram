87f72bafb6ba056c2a67da9742f754fa3d4b4e16
Abbie Vance <89212624+abbievance@users.noreply.github.com>
2023-12-11 16:10:49 -0600

    Fix clipping issues in the header

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -34,14 +34,14 @@ const Main = (props : Props) => {
 
   return (
     <div className='flex flex-col justify-start items-center border-l-2 border-r-2 w-3/4'>
-      <div className="flex flex-col lg:flex-row fixed w-1/2 justify-between items-center mt-2 header">
+      <div className="flex flex-col lg:flex-row fixed w-1/2 lg:justify-evenly justify-between items-center mt-2 header">
 
         <div className='flex mt-2 justify-center items-center text-white text-4xl'>
             <h1 className='text-center'><RiHashtag /></h1>
             <h1 className='text-center'>Huegram</h1>
         </div>
 
-        <div className='flex mt-2 justify-center items-center w-full sm:w-1/4 lg:mr-6 mr-0 search'>
+        <div className='flex mt-2 justify-center items-center w-full sm:w-1/4 search'>
             <input type="text" placeholder='Search Hues...' name="search" id="search" className='text-slate-800 rounded-lg p-1' value={searchHue}
             onChange={(e) => {
               setSearchHue(e.target.value), filterHues()
